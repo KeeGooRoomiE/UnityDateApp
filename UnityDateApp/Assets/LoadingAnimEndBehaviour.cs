c59234d12a0a247411b92022aca675d67cf84a37
KeeGooRoomiE <59682188+KeeGooRoomiE@users.noreply.github.com>
2021-11-14 15:20:02 +0700

    End skeleton, add loading screen, skeleton for app screen, player settings

---
+++ UnityDateApp/Assets/LoadingAnimEndBehaviour.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingAnimEndBehaviour : StateMachineBehaviour
+{
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.enabled = false;
+    }
+}
---
+++ UnityDateApp/Assets/Scripts/LoadingScreenBehaviour.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * FAKE LOADING SCREEN BEHAVIOUR
+ * 
+ * Alexander Gusarov - DragonGameStudios 2021
+ * 
+ */
+
+
+
+public class LoadingScreenBehaviour : MonoBehaviour
+{
+    private float totalLoadingTime;
+    [SerializeField] private float loadTimeMin;
+    [SerializeField] private float loadTimeMax;
+    [SerializeField] private Animator anim;
+    //[SerializeField] private Animation anim;
+    [SerializeField] private Image loaderImage;
+    private float stepTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalLoadingTime = Random.Range(loadTimeMin, loadTimeMax);
+        Debug.Log("//LOADING TIME IS: "+totalLoadingTime+" SEC");
+        StartCoroutine(MakeLoading());
+        stepTime = 0.12f;
+        StartCoroutine(AnimationStep(stepTime));
+    }
+
+    IEnumerator MakeLoading()
+    {
+        yield return new WaitForSeconds(totalLoadingTime);
+        Debug.Log("//LOADING DONE");
+        anim.enabled = true;
+        StartCoroutine(ClosePanel());
+    }
+
+    IEnumerator ClosePanel()
+    {
+        yield return new WaitForSeconds(1.2f);
+        Debug.Log("//CLOSING PANEL");
+        gameObject.SetActive(false);
+    }
+
+    public void RotateCircle()
+    {
+        loaderImage.transform.Rotate(new Vector3(0, 0, -30));
+        Debug.Log("// Rotating Loading circle...");
+    }
+
+    private IEnumerator AnimationStep(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        var r = Random.Range(0f, 1f);
+        if (r <= 0.85f)
+        {
+            RotateCircle();
+            StartCoroutine(AnimationStep(stepTime));
+        }
+        else
+        {
+            StartCoroutine(AnimationStep(stepTime));
+            Debug.Log("// Rotating Loading circle evaded...");
+        }
+    }
+
+}
